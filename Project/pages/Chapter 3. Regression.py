bf897d5069c403d2c34741af2f3babfcc1909a18
Hongzhi Wen <wenguanjung@aliyun.com>
2023-12-06 05:32:28 -0500

    Update Chapter 3. Regression.py

--- Project/pages/Chapter 3. Regression.py
+++ Project/pages/Chapter 3. Regression.py
@@ -82,6 +82,7 @@ with st.container():
     * Kernel ridge regression (KRR)
     * Lasso regression
     * Elastic net
+       
        Among these models, Elastic net has the most regularization and least model complexity.
     """)
     tabs = st.tabs([('KRR', 'Lasso', 'Elastic'))
